cf1764c7a0dd328473bc89e0126ccb007503f9a9
irfan_em <imachmud97@gmail.com>
2023-10-06 00:11:35 +0700

    membuat project mvc pzn

---
+++ app/App/Router.php
@@ -0,0 +1,29 @@
+<?php 
+
+namespace Irfanm\Belajar\PHP\MVC\App;
+
+class Router
+{
+
+    private static array $routes = [];
+
+    public static function add(string $method, string $path, string $controller, string $function): void
+    {
+
+        self::$routes[] = [
+            'method' => $method,
+            'path' => $path,
+            'controller' => $controller,
+            'function' => $function
+        ];
+
+    }
+
+    public static function run(): void
+    {
+
+
+
+    }
+
+}
